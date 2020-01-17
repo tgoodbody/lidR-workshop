aed6b23cca84090e9b7fdbd23fe92e0b0b2e3e49
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2020-01-17 11:38:12 -0500

    Create 2 hours version of the workshop

---
+++ 2 days/Day 1/1-7 lasf.r
@@ -0,0 +1,218 @@
+library(lidR)
+rm(list = ls(globalenv()))
+
+# ======================================
+#         OTHER LASFUNCTIONS
+# ======================================
+
+# We have already seen several function families:
+#
+# - io: to read and write LAS object in las/laz files
+# - lasfilter*: to select points of interest (return LAS objects)
+# - lasclip*: to select regions of interest (return LAS objects)
+# - grid_*: to rasterize the point cloud (return Raster* objects)
+#
+# We now introduce the other functions las* that return LAS objects
+
+las = readLAS("data/MixedEucaNat_normalized.laz")
+
+# A. lasmergespatial: merge geographic data with the point cloud
+# ==========================================================
+
+# 1. With a shapefile of polygons
+# -------------------------------
+
+# Load a shapefile
+
+eucalyptus = shapefile("data/shapefiles/MixedEucaNat.shp")
+
+# Merge with the point cloud
+
+lasc = lasmergespatial(las, eucalyptus, "in_plantation")
+lasc
+
+# Visualize
+
+plot(lasc, color = "in_plantation")
+
+# Do something: here, for the example, we simply filter the points. You can imagine any application
+
+not_plantation = lasfilter(lasc, in_plantation == FALSE)
+plot(not_plantation)
+
+# 2. With a raster
+# ----------------------------
+
+# In the past it was possible to get an easy access to the google map API via R to get satellite
+# images. Former example consisted in RGB colorization of the point cloud. It is no longer possible
+# to access to the google API without a registration key. Thus I replaced the RGB colorization by a
+# less nice example.
+
+# Make a raster. Here a CHM
+
+chm = grid_canopy(las, 1, p2r())
+plot(chm, col = height.colors(50))
+
+# Merge with the point cloud
+
+lasc = lasmergespatial(las, chm, "hchm")
+lasc
+
+# Do something. Here for the example we simply filter a layer below the canopy.
+# You can imagine any application. RGB colorization was one of them.
+
+layer = lasfilter(lasc, Z > hchm - 1)
+plot(layer)
+
+not_layer = lasfilter(lasc, Z < hchm - 1)
+plot(not_layer)
+
+# 3. Extra example that works only with my modified version of dismo
+#    and a google map API key.
+# ------------------------------------------------------------------
+
+library(dismo)
+
+bbox  <- extent(las)
+proj4 <- proj4string(las)
+
+r <- raster()
+extent(r) <- bbox
+proj4string(r) <- proj4
+
+gm  <- gmap(x = r, type = "satellite", scale = 2, rgb = TRUE)
+plotRGB(gm)
+
+gm <- projectRaster(gm, crs = proj4)
+
+las <- lasmergespatial(las, gm)
+
+plot(las, color = "RGB")
+
+lascheck(las)
+
+
+# B. Memory usage consideration
+# ===============================
+
+pryr::object_size(las)
+pryr::object_size(lasc)
+
+pryr::object_size(las, lasc)
+
+# This is true for any lasfunction that does not change the number of points i.e
+# almost all the functions but lasfilter*, lasclip* and lascheck
+
+# C. lassmooth: point cloud based smoothing
+# =========================================
+
+# Smooth the point cloud
+lass = lassmooth(las, 4)
+
+plot(lass)
+
+# It is not really useful. It may become interesting combined with lasfiltersurfacepoints
+
+lassp = lasfiltersurfacepoints(las, 0.5)
+lass = lassmooth(lassp, 2)
+
+plot(lassp)
+plot(lass)
+
+# D. lasadddata: add data to a LAS object
+# ========================================
+
+A <- runif(nrow(las@data), 10, 100)
+
+# Forbidden
+
+las$Amplitude <- A
+
+# The reason is to force the user to read the documentation of lasadddata
+?lasadddata
+
+# lasadddata does what you might expect using <-
+
+las_new = lasadddata(las, A, "Amplitute")
+
+# But the header is not updated
+
+las_new@header
+
+# lasaddextrabyte actually adds data in a way that enables the data to be written in las files
+
+las_new = lasaddextrabytes(las, A, "Amplitude", "Pulse amplitude")
+
+# The header has been updated
+
+las_new@header
+
+# E: lasground: segment ground points
+# =======================================
+
+las = readLAS("data/MixedEucaNat.laz")
+plot(las)
+
+# The original file contains an outlier
+
+hist(las$Z, n = 30)
+range(las$Z)
+
+# Read the file skipping the outlier
+
+las = readLAS("data/MixedEucaNat.laz", filter = "-drop_z_below 740")
+plot(las)
+
+# The file is already classified. For the purpose of the example we can clear this classification
+
+las$Classification = 0 # Error, explain why.
+
+plot(las, color = "Classification")
+
+# Segment the ground points with lasground
+
+las = lasground(las, csf(rigidness = 2.5))
+
+plot(las, color = "Classification")
+
+ground = lasfilterground(las)
+plot(ground)
+
+# F. lasnormalize: remove topography
+# ===================================
+
+# 1. With a DTM
+# -------------
+
+dtm = grid_terrain(las, 1, tin())
+
+plot(dtm, col = height.colors(50))
+plot_dtm3d(dtm)
+
+lasn = lasnormalize(las, dtm)
+
+plot(lasn)
+
+lascheck(lasn)
+
+
+# 2. Without a DTM
+# -----------------
+
+lasn = lasnormalize(las, tin())
+plot(lasn)
+
+lascheck(lasn)
+
+# Explain the difference betwwen the two methods
+
+# G. Other las* functions
+# ========================
+
+# lastrees (see next section)
+# lassnags
+# lasunsmooth
+# lasunormalize
+# lasflightline
+# laspulse
+# ...
---
+++ 2 days/Day 2/2-2 clip.R
@@ -0,0 +1,139 @@
+library(lidR)
+rm(list = ls(globalenv()))
+
+# ======================================
+#    SELECTION OF REGIONS OF INTEREST
+# ======================================
+
+ctg = catalog("data/Farm_A/")
+
+# A. Select simple geometries
+# =============================
+
+x = 207830
+y = 7357410
+
+plot(ctg)
+points(x,y)
+
+subset = lasclipCircle(ctg, x, y, 30)
+
+plot(subset, clear_artifact = TRUE)
+
+subset2 = lasclipRectangle(ctg, x, y, x + 50, y + 60)
+
+plot(subset2, clear_artifact = TRUE)
+
+x = c(207846, 208131, 208010, 207852)
+y = c(7357315, 7357537, 7357372, 7357548)
+
+plot(ctg)
+points(x,y)
+
+subsets1 = lasclipCircle(ctg, x, y, 30)
+
+plot(subsets1[[1]], clear_artifact = TRUE)
+plot(subsets1[[3]], clear_artifact = TRUE)
+
+lascheck(subsets1[[1]])
+lascheck(subsets1[[3]])
+
+# B. Introduction to catalog processing engine
+# ============================================
+
+?lasclip
+
+# 1. Propagate the filter option to readLAS
+# -----------------------------------------
+
+opt_filter(ctg) <- "-drop_withheld"
+
+subsets1 = lasclipCircle(ctg, x, y, 30)
+
+lascheck(subsets1[[1]])
+lascheck(subsets1[[3]])
+
+# 2. Propagate the select option to readLAS
+# -----------------------------------------
+
+opt_select(ctg) <- "xyz"
+
+subsets1 = lasclipCircle(ctg, x, y, 30)
+
+subsets1[[1]]
+
+# 3. Use multicore
+# -----------------------------------------
+
+opt_cores(ctg) <- 2
+
+subsets1 = lasclipCircle(ctg, x, y, 30)
+
+# 4. Look at current options
+# -----------------------------------------
+
+summary(ctg)
+
+# 5. Do not return the output to R
+# ----------------------------------------
+
+opt_output_files(ctg) <- "data/output/test_lasclip1/plot_{ID}"
+
+summary(ctg)
+
+subsets1 = lasclipCircle(ctg, x, y, 30)
+
+subsets1
+
+plot(subsets1)
+
+las = readLAS(subsets1$filename[2])
+
+# lidar data do not match with satellite data (probably different times)
+plot(las)
+
+# D. Extraction of complex geometries
+# ====================================
+
+ctg      = catalog("data/Farm_A/")
+planting = shapefile("data/shapefiles/Farm_A.shp")
+
+plot(planting)
+plot(ctg, map = F, add = T)
+
+# 1. clip from the shapefile and return the output in R (not recommended)
+# ---------------------------------------------------
+
+# Do not run this snipets, it will load 1 GB of data in memory
+
+las_planting = lasclip(ctg, planting)
+las_planting = Filter(Negate(is.empty), las_planting)
+
+plot(las_planting[[2]])
+
+# 2. clip from the shapefile and write in files (recommended)
+# ---------------------------------------------------
+
+opt_output_files(ctg) <- "data/output/test_lasclip2/{OBJECTID}_{SUBTALHAO}_{FAZENDA}"
+opt_laz_compression(ctg) <- TRUE
+opt_cores(ctg) <- 4
+
+summary(ctg)
+
+new_ctg = lasclip(ctg, planting) # a bit long computation
+
+plot(new_ctg)
+
+new_ctg$filename
+
+las = readLAS(new_ctg$filename[3], select = "xyz")
+plot(las, bg = "white")
+
+# E. Exercise: extract a ground inventory
+# ========================================
+
+# The shapefile in data/shapefiles/ named ground_inventories.shp contains centers of plots
+# Extract each plot with a radius of 20 m
+
+ctg = catalog("data/Farm_A/")
+plot_centers = shapefile("data/shapefiles/ground_inventories.shp")
